a0c6f370bedbc209b891c6ebf2d72f12f9214875
maib-ecomm <117263659+maib-ecomm@users.noreply.github.com>
2022-11-07 14:12:50 +0200

    Update maib.php

--- upload/catalog/language/en-gb/extension/payment/maib.php
+++ upload/catalog/language/en-gb/extension/payment/maib.php
@@ -3,7 +3,7 @@
 $_['text_title'] = 'Credit / Debit Card (via MAIB)';
 $_['text_maib_error'] = 'Payment failed.';
 $_['text_maib_pending'] = 'Payment still in pending state. Please check later or contact site administrator.';
-$_['error_no_currency'] = 'Only following currency codes are supported: EUR, MDL.';
+$_['error_no_currency'] = 'Only following currency codes are supported: USD, EUR, MDL.';
 $_['error_no_transaction_id'] = 'Unable to register payment transaction. Try again or use another payment method for the order.';
 $_['error_no_payment'] = 'Payment failed. Please try again or use another payment method.';
 ?>
